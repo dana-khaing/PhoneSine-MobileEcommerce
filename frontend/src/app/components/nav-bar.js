3afb27106a63b9a6f21c32e940c00affa7280eac
Dana Khaing <dana1352000.uk@gmail.com>
2024-03-02 00:42:49 +0000

    resloved useless 'X' buttom to work and refactor login page to component

--- frontend/src/app/components/nav-bar.js
+++ frontend/src/app/components/nav-bar.js
@@ -1,12 +1,9 @@
 "use client";
 
-import { CircleUserRound, ShoppingBag, Search } from "lucide-react";
-import { useState } from "react";
+import { ShoppingBag, Search } from "lucide-react";
 import { NavList } from "./navItem/navList";
-import LoginPage from "../login/page";
+import Login from "./login/logInCard";
 export default function NavBar() {
-  const [isClicked, setIsClicked] = useState(false);
-
   return (
     <nav className="flex justify-between items-center px-[3vw] py-7">
       <h1 className="text-2xl font-bold pr-2 ">Phone Sine</h1>
@@ -19,28 +16,13 @@ export default function NavBar() {
           type="text"
           placeholder="Search"
         />
-
         <button className="pl-1 pr-1.5">
           <Search className="w-5 h-5  text-neutral-600 hover:text-black" />
         </button>
-
         <button className="p-1.5">
           <ShoppingBag className="w-5 h-5  text-neutral-600 hover:text-black" />
         </button>
-
-        <button
-          onClick={() => {
-            setIsClicked(true);
-          }}
-          className="p-1.5"
-        >
-          <CircleUserRound className="w-5 h-5  text-neutral-600 hover:text-black" />
-        </button>
-        {isClicked && (
-          <div className="fixed inset-0 flex items-center justify-center z-10  bg-gray-800 bg-opacity-40 backdrop-blur-md">
-            <LoginPage />
-          </div>
-        )}
+        <Login />
       </div>
     </nav>
   );
